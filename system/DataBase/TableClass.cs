ec867ea5d75ea0768c8c003cacae54a5ce1b1ec8
disappeared <shenruizong@gmail.com>
2013-04-12 01:56:44 +0800

    修改注释，增加添加桌台方法

--- system/DataBase/TableClass.cs
+++ system/DataBase/TableClass.cs
@@ -11,12 +11,32 @@ namespace DataBase
         /// <summary>
         /// 查询所有信息
         /// </summary>
-        /// <returns></returns>
+        /// <returns>返回一个带数据的虚拟表</returns>
         public DataTable SelectAll()
         {
             DatabaseDataSetTableAdapters.tablesTableAdapter tableAp = new DatabaseDataSetTableAdapters.tablesTableAdapter();
             DataTable dt = tableAp.GetData();
             return dt;
+        }
+        /// <summary>
+        /// 添加桌台
+        /// </summary>
+        /// <param name="table_name">桌台名</param>
+        public void AddTable(string table_name)
+        {
+            try
+            {
+                DatabaseDataSetTableAdapters.tablesTableAdapter tableAp = new DatabaseDataSetTableAdapters.tablesTableAdapter();
+                DatabaseDataSet.tablesDataTable tablesDt = new DatabaseDataSet.tablesDataTable();
+                DataRow row = tablesDt.NewRow();
+                row["name"] = table_name;
+                tablesDt.Rows.Add(row);
+                tableAp.Update(tablesDt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
