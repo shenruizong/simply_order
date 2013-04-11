d765affe8d306366bba2f32d1f7714b33e2ac5f2
disappeared <shenruizong@gmail.com>
2013-04-12 01:48:52 +0800

    添加查询所有信息操作

--- system/DataBase/TableClass.cs
+++ system/DataBase/TableClass.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 
 namespace DataBase
 {
-    class TableClass
+    public class TableClass
     {
+        /// <summary>
+        /// 查询所有信息
+        /// </summary>
+        /// <returns></returns>
+        public DataTable SelectAll()
+        {
+            DatabaseDataSetTableAdapters.tablesTableAdapter tableAp = new DatabaseDataSetTableAdapters.tablesTableAdapter();
+            DataTable dt = tableAp.GetData();
+            return dt;
+        }
     }
 }
