88f26eedd691df27e55895f3e62fbbdd87d01753
disappeared <shenruizong@gmail.com>
2013-04-18 01:28:55 +0800

    添加数据库结构，添加菜表。添加菜表操作方法。添加菜表操作类

---
+++ system/DataBase/DishModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace DataBase
+{
+    public class DishModel
+    {
+        public DataTable SelectAll()
+        {
+            DatabaseDataSetTableAdapters.dishTableAdapter dish = new DatabaseDataSetTableAdapters.dishTableAdapter();
+            DataTable dt = dish.GetData();
+            return dt;
+        }
+    }
+}
