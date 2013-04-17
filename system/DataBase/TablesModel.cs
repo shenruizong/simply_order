917c7b07f3ae03cd646d0d8fcf21af027ea977f4
disappeared <shenruizong@gmail.com>
2013-04-17 13:00:17 +0800

    添加数据库方法

---
+++ system/DataBase/TablesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace DataBase
+{
+    public class TablesModel
+    {
+        public DataTable SelectAll()
+        {
+            DatabaseDataSetTableAdapters.tablesTableAdapter tables = new DatabaseDataSetTableAdapters.tablesTableAdapter();
+            DataTable dt = tables.GetData();
+            return dt;
+        }
+    }
+}
