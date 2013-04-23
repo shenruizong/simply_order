afd966a073923ff78deff061141ee02fa2d81aa1
disappeared <shenruizong@gmail.com>
2013-04-23 18:49:38 +0800

    添加按键无效化

--- system/server/MainWindow.xaml.cs
+++ system/server/MainWindow.xaml.cs
@@ -23,21 +23,48 @@ namespace server
         {
             InitializeComponent();
         }
-
+        private Tables.TablesWindow TablesWindow = new Tables.TablesWindow();
+        private Dish.DishWindow dishWindow = new Dish.DishWindow();
+        private Order.OrdersWindow OrdersWindow = new Order.OrdersWindow();
         private void DishButton_Click(object sender, RoutedEventArgs e)
         {
-            new Dish.DishWindow().Show();
+            if (dishWindow.IsVisible)
+            {
+                dishWindow.Activate();
+            }
+            else
+            {
+                dishWindow = new Dish.DishWindow();
+                dishWindow.Show();
+            }
+
         }
 
         private void TablesButton_Click(object sender, RoutedEventArgs e)
         {
-            new Tables.TablesWindow().Show();
+            if (TablesWindow.IsVisible)
+            {
+                TablesWindow.Activate();
+            }
+            else
+            {
+                TablesWindow = new Tables.TablesWindow();
+                TablesWindow.Show();
+            }
         }
 
         private void OrderButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-        	// 在此处添加事件处理程序实现。
-			new Order.OrdersWindow().Show();
+            // 在此处添加事件处理程序实现。
+            if (OrdersWindow.IsVisible)
+            {
+                OrdersWindow.Activate();
+            }
+            else
+            {
+                OrdersWindow = new Order.OrdersWindow();
+                OrdersWindow.Show();
+            }
         }
     }
 }
